ddff256ae7be02e676d27235b7e991b5623eed6b
EUye9IM <euye9im@euye9im-yoga14s.(none)>
2022-06-02 23:37:42 +0800

    http服务

---
+++ src/server/README.md
@@ -0,0 +1,2 @@
+run.bat 直接运行
+build.bat 生成二进制文件
---
+++ src/server/config.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v3"
+)
+
+var Cfg struct {
+	Debug bool
+	App   struct {
+		Resource string
+	}
+}
+
+func init() {
+	var config_path string
+	flag.StringVar(&config_path, "c", "conf.yml", "set path to configuration file")
+	flag.Parse()
+
+	conf_file, err := ioutil.ReadFile(config_path)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = yaml.Unmarshal(conf_file, &Cfg)
+	if err != nil {
+		panic(err.Error())
+	}
+}
