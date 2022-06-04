8775538b5200ed2af9c84eecbfea93e17bc7ce5f
EUye9IM <euye9im@euye9im-yoga14s.(none)>
2022-06-05 00:54:21 +0800

    交换SDP

--- src/README.md
+++ src/README.md
@@ -1,2 +1,6 @@
 run.bat 直接运行
 build.bat 生成二进制文件
+
+信令依赖于 websocket
+
+![](https://img-blog.csdnimg.cn/20200618134458725.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L2FnZ3Jlc3Nz,size_16,color_FFFFFF,t_70)
--- src/webrtc.go
+++ src/webrtc.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"github.com/pion/webrtc/v3"
 )
 
 var upgrader = websocket.Upgrader{
@@ -26,20 +28,94 @@ func WebsocketServer(c *gin.Context) {
 	defer log.Println("Websocket Close")
 	defer delete(conn_set, conn)
 
+	peerConnection, err := webrtc.NewPeerConnection(webrtc.Configuration{})
+	if err != nil {
+		log.Panicln("NewPeerConnection error: " + err.Error())
+	}
+	defer func() {
+		if cErr := peerConnection.Close(); cErr != nil {
+			log.Panicln("cannot close peerConnection: %v\n" + cErr.Error())
+		}
+	}()
+
+	peerConnection.OnConnectionStateChange(func(s webrtc.PeerConnectionState) {
+		log.Printf("Peer Connection State has changed: %s\n", s.String())
+
+		if s == webrtc.PeerConnectionStateFailed {
+			// Wait until PeerConnection has had no network activity for 30 seconds or another failure. It may be reconnected using an ICE Restart.
+			// Use webrtc.PeerConnectionStateDisconnected if you are interested in detecting faster timeout.
+			// Note that the PeerConnection may come back from PeerConnectionStateDisconnected.
+			log.Println("Peer Connection has gone to failed exiting")
+		}
+	})
+
+	// Register data channel creation handling
+	peerConnection.OnDataChannel(func(d *webrtc.DataChannel) {
+		log.Printf("New DataChannel %s %d\n", d.Label(), d.ID())
+
+		// Register channel opening handling
+		d.OnOpen(func() {
+			log.Printf("Data channel '%s'-'%d' open. Random messages will now be sent to any connected DataChannels every 5 seconds\n", d.Label(), d.ID())
+
+			// for range time.NewTicker(5 * time.Second).C {
+			// 	message := signal.RandSeq(15)
+			// 	fmt.Printf("Sending '%s'\n", message)
+
+			// 	// Send the message as text
+			// 	sendErr := d.SendText(message)
+			// 	if sendErr != nil {
+			// 		panic(sendErr)
+			// 	}
+			// }
+		})
+
+		// Register text message handling
+		d.OnMessage(func(msg webrtc.DataChannelMessage) {
+			log.Printf("Message from DataChannel '%s': '%s'\n", d.Label(), string(msg.Data))
+			d.SendText("Hi, " + d.Label() + ". You just say: '" + string(msg.Data) + "'.")
+		})
+	})
+
 	conn_set[conn] = true
 	for {
-		msg_type, content, err := conn.ReadMessage()
+		_, content, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("Websocket Read Error: " + err.Error())
 			break
 		}
-		log.Println("Websocket Read: " + string(content))
-		for connect := range conn_set {
-			err = connect.WriteMessage(msg_type, content)
+		offer := webrtc.SessionDescription{}
+		err = json.Unmarshal(content, &offer)
+		if err != nil {
+			log.Println("Websocket Read is not offer: " + err.Error() + "\n" + string(content))
+			continue
+		}
+		if offer.Type != webrtc.SDPTypeOffer {
+			log.Println("Websocket Read is not offer: " + string(content))
+			continue
+		} else {
+			log.Println("receive offer: " + offer.SDP)
+
+			// Set the remote SessionDescription
+			err = peerConnection.SetRemoteDescription(offer)
 			if err != nil {
-				log.Println("Websocket Write Error: " + err.Error())
-				break
+				log.Panicln(err)
 			}
+
+			// Create an answer
+			answer, err := peerConnection.CreateAnswer(nil)
+			if err != nil {
+				log.Panicln(err)
+			}
+
+			// Sets the LocalDescription, and starts our UDP listeners
+			err = peerConnection.SetLocalDescription(answer)
+			if err != nil {
+				log.Panicln(err)
+			}
+
+			//send answer back
+			conn.WriteJSON(answer)
+
 		}
 	}
 }
